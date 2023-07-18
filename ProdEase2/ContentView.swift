987134558e803c8effbb63eed221df95d32c7edd
Lindsey Kwok <kwoklindsey0@gmail.com>
2023-07-18 10:55:56 -0700

    created prodEase open page

--- ProdEase2/ContentView.swift
+++ ProdEase2/ContentView.swift
@@ -1,21 +1,40 @@
 //
 //  ContentView.swift
-//  ProdEase2
+//  prodEase
 //
 //  Created by Lindsey Kwok on 7/18/23.
-//
+//j
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+                .frame(height: 200)
+            
+            Text("ProdEase")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+//                .foregroundColor(Color(red: 6.3, green: 14.5, blue: 131.0))
+            Spacer()
+                .frame(height: 10)
+            Text("Productivity Made Easier")
+                .fontWeight(.bold)
+            
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height : 200)
+            
+            Text("Tap anywhere to begin")
+            
+            Image("bottomPicture")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height : 300)
+
         }
-        .padding()
     }
 }
 
