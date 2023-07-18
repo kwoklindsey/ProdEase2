9a9799b9bbc825f3a81ed21aa329e571d57a6577
Lindsey Kwok <kwoklindsey0@gmail.com>
2023-07-18 10:53:47 -0700

    Initial Commit

---
+++ ProdEase2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ProdEase2
+//
+//  Created by Lindsey Kwok on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProdEase2/ProdEase2App.swift
@@ -0,0 +1,17 @@
+//
+//  ProdEase2App.swift
+//  ProdEase2
+//
+//  Created by Lindsey Kwok on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ProdEase2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
